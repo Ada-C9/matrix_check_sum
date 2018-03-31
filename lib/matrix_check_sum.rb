4138fbff1381c680a3bb639bee442da951c80661
Wenjie <traxiaojie@gmail.com>
2018-03-31 11:04:03 -0700

    Completed matrix check sum function

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -1,7 +1,19 @@
-# Checks that for the given matrix, where number of rows are equal to number of columns
-# whether the sum of each row matches the sum of corresponding column i.e. sum
-# of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
-# If this is the case, return true. Otherwise, return false.
+# Thought process: sum up each row and column and nth index, compare if they equal
+# Time complexity: O(mn) - m is the length of matrix and n is the length of matrix[0]
+# Space complexity: O(1)
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  i = 0
+  while i < matrix.length
+    sum_row = 0
+    sum_column = 0
+    j = 0
+    while j < matrix.length
+      sum_row += matrix[i][j]
+      sum_column += matrix[j][i]
+      j += 1
+    end
+    return false if sum_row != sum_column
+    i += 1
+  end
+  return true
 end
