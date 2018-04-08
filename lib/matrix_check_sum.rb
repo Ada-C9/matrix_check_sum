c8acbf699a259fed1faafd4d340d790c65555474
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-07 19:46:09 -0700

    All tests passing

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,5 +3,23 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  size = matrix[0].length
+  x = 0
+
+  while x < size
+    i = 0
+    sum_across = 0
+    sum_down = 0
+
+    while i < size
+      sum_across += matrix[x][i]
+      sum_down += matrix[i][x]
+      i += 1
+    end
+
+    return false if sum_across != sum_down
+    x += 1
+  end
+
+  return true
 end
